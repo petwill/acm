4669636504fdec92dd5a71a5745765cfb187b3aa
petwill <sunfanyun@gmail.com>
2016-08-31 11:13:56 +0800

    Update Extended Euclidean algorithm.cpp

--- dsa/Math/Extended Euclidean algorithm.cpp
+++ dsa/Math/Extended Euclidean algorithm.cpp
@@ -7,7 +7,8 @@ int gcd(int a, int b){
     if(b == 0) return a;
     return gcd(b, a%b);
 }
-
+// Find solution of ax + by = gcd(a, b)
+// ps : x, y may be negative
 int extgcd(int a, int b, int& x, int& y){
     int d = a;
     if(b != 0) {
