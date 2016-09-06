c6979587f01e1bf3db92abdcad9eba6cc1d6608f
petwill <sunfanyun@gmail.com>
2016-09-07 02:25:13 +0800

    Create template.cpp

---
+++ dsa/Graph_Theory/tarjan/割點and點雙連通分量/template.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef DEBUG
+    #define debug(...) printf(__VA_ARGS__)
+#else
+    #define debug(...) (void)0
+#endif
+#define mp make_pair
+#define pb push_back
+#define LL long long
+#define pii pair<int,int>
+#define PII pair<long long, long long>
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+const int inf = 0x7fffffff; //beware overflow
+const LL INF = 0x7fffffffffffffff; //beware overflow
+#define mem(x, y) memset(x, (y), sizeof(x) );
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0)
+template<typename A, typename B>
+ostream& operator <<(ostream &s, const pair<A,B> &p) {
+     return s<<"("<<p.first<<","<<p.second<<")";
+}
+template<typename T>
+ostream& operator <<(ostream &s, const vector<T> &c) {
+    s << "[ ";
+    for (auto it : c) s << it << " ";
+    s << "]";
+    return s;
+}
+template<typename T>
+ostream& operator << (ostream &o, const set<T> &st) {
+    o << "{";
+    for (auto it=st.begin(); it!=st.end(); it++) o << (it==st.begin() ? "" : ", ") << *it;
+    return o << "}";
+}
+template<typename T1, typename T2>
+ostream& operator << (ostream &o, const map<T1, T2> &mp) {
+    o << "{";
+    for (auto it=mp.begin(); it!=mp.end(); it++) {
+        o << (it==mp.begin()?"":", ") << it->fi << ":" << it->se;
+    }
+    o << "}";
+    return o;
+}
+
+/** needed for tarjan **/
+// bcc is a set of edges 
+#define maxn 100005
+#define maxm 100005
+int n, m;
+struct Edge{int s, t;};
+vector<Edge> edge;
+int dfn[maxn], low[maxn];
+stack<int> st;
+bool vis[maxn];
+int Time;
+bool vis_e[maxm];
+int bcnt, vbb[maxm];
+vector<int> vb[maxm];
+vector<int> G[maxn];
+/** **/
+void tarjan(int s){
+    dfn[s] = low[s] = ++Time;
+    vis[s] = true;
+    for(int e_ind : G[s]){
+        if(!vis_e[e_ind]){
+            vis_e[e_ind] = true; st.push(e_ind);
+            int to = edge[e_ind].s + edge[e_ind].t - s;
+            if(!vis[to]){
+                tarjan(to);
+                low[s] = min(low[s], low[to]);
+                if(low[to] >= dfn[s]){
+                    vb[bcnt].clear();
+                    while(1){
+                        int t = st.top();st.pop();
+                        vbb[t] = bcnt;
+                        vb[bcnt].push_back(t);
+                        if(t == e_ind) break;
+                    }
+                    bcnt++;
+                }
+            }else
+                low[s] = min(low[s], dfn[to]);
+        }
+    }
+}
+void init_tarjan() {
+
+    mem(vis, false); mem(vis_e, false);
+    Time = bcnt = 0; edge.clear();
+    for(int i = 1; i <= n; i++) G[i].clear();
+}
+
+int main() {
+
+    cin >> n >> m;
+    init_tarjan();
+    for(int i = 0; i < m; i++) {
+        int a, b; scanf("%d %d", &a, &b);
+        edge.push_back(Edge{a,b});
+        G[a].push_back((int)edge.size()-1);
+        G[b].push_back((int)edge.size()-1);
+    }
+    tarjan(1);
+
+}
