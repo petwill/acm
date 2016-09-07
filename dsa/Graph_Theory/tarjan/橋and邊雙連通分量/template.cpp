45927a6f3326a6fc8709e084b0b088b773f1a65c
petwill <sunfanyun@gmail.com>
2016-09-08 03:01:15 +0800

    Update template.cpp

--- dsa/Graph_Theory/tarjan/橋and邊雙連通分量/template.cpp
+++ dsa/Graph_Theory/tarjan/橋and邊雙連通分量/template.cpp
@@ -20,9 +20,22 @@ void tarjan(int s, int p){
             tarjan(to, s);
             low[s] = min(low[s], low[to]);
             if( low[to] > dfn[s]) {
-                // it cut_edge
+                // is cut_edge
                 // pop stack 的過程也可以寫在這
-                // 但最後還要多判stack not empty的情況
+                // 但最後(after tarjan)還要多判stack not empty的情況
+                /*
+                if( low[to] > dfn[s]) {
+
+                in_cyc[bcnt] = st.top()!=to;
+                while(1){
+                    int t = st.top();st.pop();
+                    id[t] = bcnt;
+                    if(t == to) break;
+                }
+                bcnt++;
+                */
+            }
+
             }
         }else 
             low[s] = min(low[s], dfn[to]);
