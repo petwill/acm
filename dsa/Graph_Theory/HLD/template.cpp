e16b154c5cf34619857dd0f88a95af909b327438
petwill <sunfanyun@gmail.com>
2016-09-12 18:33:54 +0800

    Update template.cpp

--- dsa/Graph_Theory/HLD/template.cpp
+++ dsa/Graph_Theory/HLD/template.cpp
@@ -1,5 +1,50 @@
 
+struct segment_tree{
+    #define MAXN 100100
+    #define right(x) x << 1 | 1
+    #define left(x) x << 1
+    int* arr;
+    LL sum[4*MAXN];
+    const int inf = 1e9;
+
+    void pull(int ind) {
+        sum[ind] = sum[right(ind)]+sum[left(ind)];
+    };
+    /// root => 1
+    void build(int ind, int l, int r) {
+
+        if( r - l == 1) {
+            sum[ind] = 0;
+            return;
+        }
+        int mid = (l+r)>>1;
+        build( left(ind), l, mid );
+        build( right(ind), mid, r );
+        pull(ind);
+    }
+    LL query_sum(int ind, int L, int R, int ql, int qr) {
+        if( L >= qr || R <= ql ) return 0;
+        if( R <= qr && L >= ql ) {
+            return sum[ind];
+        }
+        int mid = (L+R)>>1;
+        return query_sum(left(ind), L, mid, ql, qr) + query_sum(right(ind), mid, R, ql, qr);
+    }
+    void modify(int ind, int L, int R, int ql, int qr, int x) {
+        if( L >= qr || R <= ql ) return;
+        if( R <= qr && L >= ql ) {
+            sum[ind] = x;
+            return;
+        }
+        int mid = (L+R)>>1;
+        modify(left(ind), L, mid, ql, qr, x);
+        modify(right(ind), mid, R, ql, qr, x);
+        pull(ind);
+    }
+};
+
 struct Tree{
+    segment_tree seg;
     #define MAXN 100010
     #define maxm (maxn<<1)
     int n;
@@ -89,5 +134,10 @@ struct Tree{
         find_max_son(1);
         build_link(1, 1);
     }
-
+     void modify(int a, int b, int x) {
+        // modify the path from a -> b to x
+        //( which is [ link[a] .. link[b] ] on the segment tree)
+        seg.modify(1, 1, n+1, link[a], link[b]+1, x);
+        // this segment tree uses [ 1 ..n+1 )
+    }
 }tree;
