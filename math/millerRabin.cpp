fdd1fc49c6c085623285baa8cf422a2c1e21ba23
petwill <sunfanyun@gmail.com>
2016-10-01 19:01:53 +0800

    Update millerRabin.cpp

--- math/millerRabin.cpp
+++ math/millerRabin.cpp
@@ -1,63 +1,59 @@
-#include <cstdio>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+typedef long long LL;
+LL mul(LL a, LL b, const LL mod) {
+    LL x = 0,y = a % mod;
+    while (b > 0) {
+        if ( b&1 )
+            x = (x + y) % mod;
+        y = (y * 2) % mod;
+        b >>= 1;
+    }
+    return x % mod;
+}
+/*
+LL mul(LL lhs, LL rhs, const LL mod ) {
+    return ( lhs * rhs ) % mod;
+}
+*/
+LL mypow(LL b, LL e,const LL mod) {
+    LL x = 1;
+    LL y = b;
+    while ( e ) {
+        if ( e&1 ) x = mul(x, y, mod);
+        y = mul(y, y, mod);
+        e >>= 1;
+    }
+    return x;
+}
+const int testbase[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+bool isprime(const LL p) {
+    if (p < 2) return false;
+    if (p != 2 && !(p&1) ) return false;
+    LL d = p - 1;
+    while ( !(d&1) ) d >>= 1;
+    for( int a : testbase ) {
 
-long long mul(unsigned long long a, unsigned long long b, unsigned long long mod) {
-    long long ret = 0;
-    for (a %= mod, b %= mod; b != 0; b >>= 1, a <<= 1, a = a >= mod ? a - mod : a) {
-        if (b&1) {
-            ret += a;
-            if (ret >= mod) ret -= mod;
+        LL td = d;
+        if( a >= p-1 ) return true;
+        LL st = mypow(a, td, p);
+        while ( td != p - 1 && st != 1 && st != p - 1) {
+            st = mul(st, st, p);
+            td <<= 1;
         }
+        if ( st != p - 1 && !(td&1) ) return false;
     }
-    return ret;
+    return true;
 }
-
-long long mpow2(long long x, long long y, long long mod) {
-    long long ret = 1;
-    while (y) {
-        if (y&1)
-            ret = mul(ret, x, mod);
-        y >>= 1, x = mul(x, x, mod);
-    }
-    return ret % mod;
-}
-int isPrime(long long p, int it) { // implements by miller-babin
-    if (p < 2)  return 0;
-    if (p == 2) return 1;
-    if (!(p&1)) return 0;
-    long long q = p-1, a, t;
-    int k = 0, b = 0;
-    while (!(q&1))  q >>= 1, k++;
-
-    while(it--) {
-        a = rand()%(p-4) + 2;
-        t = mpow2(a, q, p);
-        b = (t == 1) || (t == p-1);
-        for (int i = 1; i < k && !b; i++) {
-            t = mul(t, t, p);
-            if (t == p-1)
-                b = 1;
-        }
-        if (b == 0)
-            return 0;
-    }
-
-    return 1;
-}
-
-
 int main() {
-
-    int testcase;
-    scanf("%d", &testcase);
-    while (testcase--) {
-        long long n;
-        scanf("%lld", &n);
-        puts(isPrime(n, 1000)?"YES":"NO");
+    int T;
+    scanf("%d",&T);
+    while(T--) {
+        LL q;
+        scanf("%lld",&q);
+        puts(isprime(q)?"YES":"NO");
     }
     return 0;
 }
