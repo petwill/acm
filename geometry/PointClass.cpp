a334dfc6f36e872f17b8b175a952c5ffe5a18ab9
petwill <sunfanyun@gmail.com>
2016-11-07 17:54:59 +0800

    Update PointClass.cpp

--- geometry/PointClass.cpp
+++ geometry/PointClass.cpp
@@ -1,7 +1,8 @@
 const double eps = 1e-10;
 #define N 100
 struct P {
-    type x,y;
+    double x, y;
+    P(double _x=0, double _y=0) :x(_x), y(_y) {};
     void read() {
         scanf("%lf%lf",&x,&y);
     }
@@ -9,15 +10,14 @@ struct P {
         printf("%f %f\n", x, y);
     }
 } p[N];
-#define type double
+#define double double
 bool operator <( P a, P b ) { return tie(a.x,a.y)<tie(b.x,b.y); }
 P operator +( P a, P b ) { return P{a.x+b.x,a.y+b.y}; }
 P operator -( P a, P b ) { return P{a.x-b.x,a.y-b.y}; }
-P operator *(  P b, type a ) { return P{a*b.x,a*b.y}; }
-P operator /( P a, type b ) { return P{a.x/b,a.y/b}; }
-P& operator /=( P &a, type b ) { return a=a/b; }
-type operator *( P a, P b ) { return a.x*b.x+a.y*b.y; }
-type operator ^( P a, P b ) { return a.x*b.y-a.y*b.x; }
-type X( P o, P a, P b ) { return (a-o)^(b-o); }
-type dot( P o, P a, P b ) { return (a-o)*(b-o); }
-
+P operator *(  P b, double a ) { return P{a*b.x,a*b.y}; }
+P operator /( P a, double b ) { return P{a.x/b,a.y/b}; }
+P& operator /=( P &a, double b ) { return a=a/b; }
+double operator *( P a, P b ) { return a.x*b.x+a.y*b.y; }
+double operator ^( P a, P b ) { return a.x*b.y-a.y*b.x; }
+double x( P o, P a, P b ) { return (a-o)^(b-o); }
+double dot( P o, P a, P b ) { return (a-o)*(b-o); }
