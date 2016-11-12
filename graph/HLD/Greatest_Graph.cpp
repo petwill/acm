b32a5218bb619e7c3fe3afe541f584f5e2a2ea3b
petwill <sunfanyun@gmail.com>
2016-11-13 04:20:36 +0800

    Update Greatest_Graph.cpp

--- graph/HLD/Greatest_Graph.cpp
+++ graph/HLD/Greatest_Graph.cpp
@@ -1,10 +1,8 @@
+
 ///http://acm.csie.org/ntujudge/problemdata/2582.pdf
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#define DEBUG
-
 #ifdef DEBUG
     #define debug(...) printf(__VA_ARGS__)
 #else
@@ -51,23 +49,18 @@ ostream& operator << (ostream &o, const map<T1, T2> &mp) {
 }
 
 
-#define maxn 100010
-using namespace std;
-
-
-#define MAXN maxn
+#define maxn 100005
 struct segment_tree{
     #define right(x) x << 1 | 1
     #define left(x) x << 1
     int* arr;
-    int m[4*MAXN], M[4*MAXN];
-    LL sum[4*MAXN];
-    int tag[4*MAXN];
+    int m[4*maxn];
+    int tag[4*maxn];
     const int inf = 1e9;
 
     void init() {
         //memset(tag, -1, sizeof(tag));
-        fill(tag, tag+4*MAXN, inf);
+        fill(tag, tag+4*maxn, inf);
     }
     void pull(int ind) {
             m[ind] = min(m[right(ind)] ,m[left(ind)]);
@@ -102,23 +95,6 @@ struct segment_tree{
         int mid = (L+R)>>1;
         return min( query_min(left(ind), L, mid, ql, qr), query_min(right(ind), mid, R, ql, qr));
     }
-    int query_max(int ind, int L, int R, int ql, int qr) {
-        if( L >= qr || R <= ql ) return -1e9;
-         if( R <= qr && L >= ql ) {
-            return M[ind];
-        }
-        int mid = (L+R)>>1;
-        return max( query_max(left(ind), L, mid, ql, qr), query_max(right(ind), mid, R, ql, qr));
-    }
-     //int query_sum
-    LL query_sum(int ind, int L, int R, int ql, int qr) {
-        if( L >= qr || R <= ql ) return 0;
-        if( R <= qr && L >= ql ) {
-            return sum[ind];
-        }
-        int mid = (L+R)>>1;
-        return query_sum(left(ind), L, mid, ql, qr) + query_sum(right(ind), mid, R, ql, qr);
-    }
     void modify(int ind, int L, int R, int ql, int qr, int x) {
         if( L >= qr || R <= ql ) return;
         if( R <= qr && L >= ql ) {
@@ -134,166 +110,156 @@ struct segment_tree{
         modify(right(ind), mid, R, ql, qr, x);
         pull(ind);
     }
-}seg;
+};
 
-/**
-    template for 樹鍊剖分
-    careful => node index [1..n]
-    siz[v]表示以v为根的子树的节点总数。
-    dep[v]表示v的深度。
-    son[v]表示与v在同一重链上的v的儿子节点。
-    fa[v]表示v的父亲节点。
-    top[v]表示v所在链的顶端节点。
-    w[v]表示节点v在线段树中的位置。
-    siz[],son[],fa[],dep[]可以在第一遍dfs中求出来，top[]，w[]可在第二遍dfs中求出来。具体过程看代码吧。
-
-    w[root] = 0, max( w[ 1..n ] ) = n-1
-    seg_arr[0] won't be queried cause this edge actually doesn't exist
-    seg_arr[ 1..n ) is actually used
-    every edge in segment tree's index == w[ the deeper node index ]
-**/
-int siz[maxn],top[maxn],son[maxn],dep[maxn],w[maxn],fa[maxn];
-int n,m;
 int seg_arr[maxn];
-struct edge
-{
-    int to;
-    int cost;
-    int next;
-}e[maxn<<1];
-int box[maxn],cnt,tot;
-void init()
-{
-    tot=0;
-    son[0]=dep[0]=0;
-    memset(box,-1,sizeof(box));
-    cnt=0;
-}
-void add_edge(int from,int to, int cost)
-{
-    e[cnt].to=to;
-    e[cnt].cost = cost;
-    e[cnt].next=box[from];
-    box[from]=cnt++;
-}
-void dfs(int now,int pre)
-{
-    siz[now]=1;
-    fa[now]=pre;
-    son[now]=0;
-    dep[now]=dep[pre]+1;
-    int t,v;
-    for(t=box[now];t+1;t=e[t].next)
-    {
-        v=e[t].to;
-        if(v!=pre)
-        {
-            dfs(v,now);
-            siz[now]+=siz[v];
-            if(siz[son[now]]<siz[v])
-            {
-                son[now]=v;
+struct Tree{
+    segment_tree seg;
+    int n;
+    struct Edge { int u, v, c; };
+    vector<Edge> e;
+    void addEdge(int x, int y, int c) {
+        G[x].pb( SZ(e) );
+        G[y].pb( SZ(e) );
+        e.pb( Edge{x, y, c}  );
+    }
+    int siz[maxn],max_son[maxn],pa[maxn],dep[maxn];
+    /*size of subtree、index of max_son, parent index、depth*/
+    int link_top[maxn],link[maxn],timer;
+    /*chain top、index in segtree、time stamp*/
+    std::vector<int >G[maxn];
+    void init(int N) {
+        n = N;
+        e.clear();
+        for(int i = 1; i <= n; i++) G[i].clear();
+        timer=0;
+        pa[1] = 1;
+        dep[1] = 0;
+    }
+    void find_max_son(int x){
+        siz[x]=1;
+        max_son[x]=-1;
+        for(int e_ind : G[x]) {
+            int v = e[e_ind].u == x ? e[e_ind].v : e[e_ind].u ;
+            if( v == pa[x] )continue;
+            pa[v] = x; dep[v] = dep[x] + 1;
+            find_max_son(v);
+            if(max_son[x] == -1 || siz[v] > siz[max_son[x]])
+                max_son[x] = v;
+            siz[x] += siz[v];
+        }
+    }
+    void build_link(int x,int top){
+        link[x] = timer++;/*記錄x點的時間戳*/
+        link_top[x] = top;
+        if(max_son[x] != -1)
+            build_link( max_son[x], top);/*優先走訪最大孩子*/
+
+        for(int e_ind : G[x]) {
+            int v = e[e_ind].u == x ? e[e_ind].v : e[e_ind].u ;
+
+            if( v == pa[x] ) {
+                seg_arr[ link[x] ] = e[e_ind].c;
             }
+            if( v == max_son[x] || v == pa[x] )continue;
+            // edge from x => v
+            build_link(v, v);
         }
     }
-}
-void dfs2(int now,int tp)
-{
-    w[now]=tot++;
-    top[now]=tp;
-
-    if(son[now])
-        dfs2(son[now],top[now]);
-    int t,v;
-    for(t=box[now];t+1;t=e[t].next)
-    {
-        v=e[t].to;
-        if( v == fa[now])
-            seg_arr[ w[now] ] = e[t].cost;
-        else if(v!=fa[now]&&v!=son[now])
-            dfs2(v,v);
-    }
-
-}
-/*
-int query(int a,int b) {
-    int f1=top[a],f2=top[b],dist=0;
-    while(f1!=f2)
-    {
-        if(dep[f1]<dep[f2])
-        {
-            swap(f1,f2);
-            swap(a,b);
+    inline int lca(int a,int b){
+        /*求LCA，可以在過程中對區間進行處理*/
+        int ta=link_top[a],tb=link_top[b];
+        while(ta != tb){
+            if(dep[ta]<dep[tb]){
+                std::swap(ta,tb);
+                std::swap(a,b);
+            }
+            //interval [ link[ta], link[a] ]
+            a = pa[ta];
+            ta = link_top[a];
         }
-        dist+=w[a]-w[f1]+1;
-        int tmp=seg.query_sum(1,2, n+1, w[f1],w[a]+1);
-        if(tmp)
-            return -1;
-        a=fa[f1];
-        f1=top[a];
+        return dep[a] < dep[b] ? a:b;
     }
-    if(a==b)
-    return dist;//注意这里
-    else
-    {
-        if(dep[a]>dep[b])
-            swap(a,b);
-        int tmp=seg.query_sum(1,2, n+1, w[son[a]],w[b]+1);//注意这里
-        if(tmp)
-            return -1;
-        return dist+w[b]-w[a];
-    }
-}
-*/
-void modify(int a, int b, int c) {
-    if( top[a] == top[b]) {
-        int mn = min( w[a], w[b]);
-        int mx = max( w[a], w[b]);
-        seg.modify(1, 1, n, mn+1, mx+1, c);
-        return;
-    }
+    
+    int modify(int a,int b, int c){
 
-    if(dep[top[a]] < dep[top[b]])
-        swap(a, b);
-    seg.modify( 1, 1, n, w[top[a]], w[a] + 1, c);
-    modify( fa[top[a]], b, c);
-}
-/** end template **/
+        int ta=link_top[a], tb=link_top[b];
+        while(ta != tb){
+            if(dep[ta]<dep[tb]){
+                std::swap(ta,tb);
+                std::swap(a,b);
+            }
+            //interval [ link[ta],link[a] ]
+            //same interval if operate on edges
+            seg.modify(1, 1, n, link[ta], link[a]+1, c);
+            a = pa[ta];
+            ta = link_top[a];
+        }
+        //a, b are on the same chain
+        if( a == b ) ; // interval [ link[a], link[a] ], if operate on edges => no edge
+        else {
+            if(dep[a]>dep[b])
+                swap(a,b);
+            //interval [ link[a],link[b] ]
+            // if operate on edges ==> [ link[ max_son[a] ], link[b] ]
+            seg.modify(1, 1, n, link[ max_son[a] ], link[b]+1, c);
+        }
+    }
+    /*
+    void modify(int a, int b, int c) {
+        if( a==b ) return;
+        if( link_top[a] == link_top[b]) {
+            if( dep[a] > dep[b] ) swap(a, b);
+            seg.modify(1, 1, n, link[a]+1, link[b]+1, c);
+            assert( link[a]+1 == link[ max_son[a]] );
+            return;
+        }
 
-/**
-#define maxm maxn
-//sometimes needed
-int Edge[maxm][2];
-**/
+        if(dep[link_top[a]] < dep[link_top[b]])
+            swap(a, b);
+        // a is the node with deeper link_top
+        seg.modify( 1, 1, n, link[link_top[a]], link[a] + 1, c);
+        modify( pa[link_top[a]], b, c);
+    }
+    */
+
+    /// Heavy Light Decomposition
+    void HLD() {
+        // root is indexed 1 here !
+        find_max_son(1);
+        build_link(1, 1);
+    }
+}tree;
+
 
 int main() {
     int T;cin>>T;
     while(T--) {
-
+        int n,m ;
         scanf("%d %d",&n, &m);
-        init();
         int ans = 0;
+        tree.init(n);
         for(int i=0;i<n-1;i++) {
             int a, b, c;
             scanf("%d%d%d",&a,&b,&c);
             //a--, b--; be careful here
-            add_edge(a, b, c);
-            add_edge(b, a, c);
+            tree.addEdge(a, b, c);
             ans += c;
         }
-        dfs(1,0);
-        dfs2(1,1);
+        tree.HLD();
 
-        seg.arr = seg_arr;
-        seg.init();
-        seg.build(1,1,n);
+//        for(int i=1;i<=n;i++)
+//            printf("%d %d\n", i, tree.link[i]);
+        tree.seg.arr = seg_arr;
+        tree.seg.init();
+        tree.seg.build(1, 1, n);
 
         for(int i = n-1; i < m; i++) {
             int a, b, c;
             scanf("%d%d%d",&a,&b,&c);
-            modify(a, b, c);
+            tree.modify(a, b, c);
         }
-        for(int i = 1; i < n; i++) ans -= seg.query_min(1, 1, n, i, i+1);
+        for(int i = 1; i < n; i++) ans -= tree.seg.query_min(1, 1, n, i, i+1);
         printf("%d\n", ans);
     }
     return 0;
