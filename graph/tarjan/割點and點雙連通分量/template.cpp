f22f4e27b526e43af32f072229ffb689217b1d39
petwill <sunfanyun@gmail.com>
2016-11-24 17:07:51 +0800

    Update template.cpp

--- graph/tarjan/割點and點雙連通分量/template.cpp
+++ graph/tarjan/割點and點雙連通分量/template.cpp
@@ -1,51 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
-#ifdef DEBUG
-    #define debug(...) printf(__VA_ARGS__)
-#else
-    #define debug(...) (void)0
-#endif
-#define mp make_pair
-#define pb push_back
-#define LL long long
-#define pii pair<int,int>
-#define PII pair<long long, long long>
-#define fi first
-#define se second
-#define all(x) (x).begin(),(x).end()
-#define SZ(x) ((int)(x).size())
-const int inf = 0x7fffffff; //beware overflow
-const LL INF = 0x7fffffffffffffff; //beware overflow
-#define mem(x, y) memset(x, (y), sizeof(x) );
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0)
-template<typename A, typename B>
-ostream& operator <<(ostream &s, const pair<A,B> &p) {
-     return s<<"("<<p.first<<","<<p.second<<")";
-}
-template<typename T>
-ostream& operator <<(ostream &s, const vector<T> &c) {
-    s << "[ ";
-    for (auto it : c) s << it << " ";
-    s << "]";
-    return s;
-}
-template<typename T>
-ostream& operator << (ostream &o, const set<T> &st) {
-    o << "{";
-    for (auto it=st.begin(); it!=st.end(); it++) o << (it==st.begin() ? "" : ", ") << *it;
-    return o << "}";
-}
-template<typename T1, typename T2>
-ostream& operator << (ostream &o, const map<T1, T2> &mp) {
-    o << "{";
-    for (auto it=mp.begin(); it!=mp.end(); it++) {
-        o << (it==mp.begin()?"":", ") << it->fi << ":" << it->se;
-    }
-    o << "}";
-    return o;
-}
-
 //    regard every vbcc as a set of edges
+//    so vb[i] is a vector that contains a set of edge indexes
 /** needed for tarjan **/
 #define maxn 100005
 #define maxm 100005
