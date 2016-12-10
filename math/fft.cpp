35edef3dcfa2ae43365b9c67214fe92de90cc87a
petwill <sunfanyun@gmail.com>
2016-12-10 18:11:23 +0800

    Update fft.cpp

--- math/fft.cpp
+++ math/fft.cpp
@@ -1,30 +1,50 @@
+//pEcaveros
+// const int MAXN = 262144;
+// (must be 2^k)
+// before any usage, run pre_fft() first
+// 
+// To implement poly. multiply:
+//
+// fft( n , a );
+// fft( n , b );
+// for( int i = 0 ; i < n ; i++ )
+//   c[ i ] = a[ i ] * b[ i ];
+// fft( n , c , 1 );
+//
+// then you have the result in c :: [cplx]
 typedef long double ld;
-/* N must be 2^k and greater than array.size()
- * FFT( a );
- * FFT( b );
- * for(int i = 0; i<N; ++i) c[i] = conj(a[i] * b[i]);
- * FFT( c );
- * for(int i = 0; i<N; ++i) c[i] = conj(c[i]);
- * for(int i = 0; i<N; ++i) c[i] /= N;
- */
-void FFT(vector< complex<ld> >& v) {
-    int N = v.size();
-    for(int i = 1, j = 0; i<N; ++i) {
-        for(int k = N>>1; !((j^=k)&k); k>>=1);
-        if(i>j) swap(v[i],v[j]);
+typedef complex<ld> cplx;
+const ld PI = acosl(-1);
+const cplx I(0, 1);
+cplx omega[MAXN+1];
+void pre_fft(){
+  for(int i=0; i<=MAXN; i++)
+    omega[i] = exp(i * 2 * PI / MAXN * I);
+}
+// n must be 2^k
+void fft(int n, cplx a[], bool inv=false){
+  int basic = MAXN / n;
+  int theta = basic;
+  for (int m = n; m >= 2; m >>= 1) {
+    int mh = m >> 1;
+    for (int i = 0; i < mh; i++) {
+      cplx w = omega[inv ? MAXN-(i*theta%MAXN)
+                         : i*theta%MAXN];
+      for (int j = i; j < n; j += m) {
+        int k = j + mh;
+        cplx x = a[j] - a[k];
+        a[j] += a[k];
+        a[k] = w * x;
+      }
     }
-    for(int k = 2; k<=N; k<<=1) {
-        ld w = -2.0*pi/k;
-        complex<ld> deg(cos(w),sin(w));
-        for(int j = 0; j<N; j+=k) {
-            complex<ld> theta(1,0);
-            for(int i = j; i<j+k/2; ++i) {
-                complex<ld> a = v[i];
-                complex<ld> b = v[i+k/2]*theta;
-                v[i] = a+b;
-                v[i+k/2] = (a-b);
-                theta *= deg;
-            }
-        }
-    }
+    theta = (theta * 2) % MAXN;
+  }
+  int i = 0;
+  for (int j = 1; j < n - 1; j++) {
+    for (int k = n >> 1; k > (i ^= k); k >>= 1);
+    if (j < i) swap(a[i], a[j]);
+  }
+  if (inv)
+    for (i = 0; i < n; i++)
+      a[i] /= n;
 }
